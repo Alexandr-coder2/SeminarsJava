9fbad073a9e37274c7864577419c73cab8e29e21
alexandr <spirin.alexandr@gmail.com>
2023-06-20 22:19:00 +0500

    lek

--- lekcia_1.java
+++ lekcia_1.java
@@ -1,8 +1,11 @@
-/** * Program */
-public class lekcia_1 
-{
-    public static void main(String[] args) 
-    {
-        System.out.println("Goodbye world");
-    }
-}
+// /** * Program */
+// public class lekcia_1 
+// {
+//     public static void main(String[] args) 
+//     {
+//         System.out.println("Goodbye world");
+//     }
+// }
+
+
+
---
+++ program.java
@@ -0,0 +1,16 @@
+// public class program {
+//     public static void main(String[] args) {
+//         System.err.println("Goodbyel world");
+//     }
+// }
+
+// class Program {  public static void main(String[] args) {      short age = 10;      int salary = 123456;      System.out.println(age);        //10      System.out.println(salary);     //123456  } }
+
+class Program {
+    public static void main(String[] args) {
+        short age = 10;
+        int salary = 123456;
+        System.out.println(age);
+    }
+
+}// 10 System.out.println(salary); //123456 } }
